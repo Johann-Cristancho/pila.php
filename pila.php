f13bb38a1d12477d87535607ebeedb7f305744a9
Johann Cristancho <johancri2010@gmail.com>
2025-05-08 21:43:54 -0500

    first commit

---
+++ pila.php
@@ -0,0 +1,14 @@
+<?php
+$pila= array();
+array_push($pila,"sword");
+array_push($pila,"shield");
+array_push($pila,"staff");
+array_push($pila,"cristals");
+array_push($pila,"horse");
+array_push($pila,"lighting");
+array_push($pila,"hammer");
+
+
+$ultimo_en_pila =array_pop($pila);
+echo "ultimo elemento :",$ultimo_en_pila;
+?>
